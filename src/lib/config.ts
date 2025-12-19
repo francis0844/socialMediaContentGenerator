8dbb77f79fee750cd50ff9e6be9dd8d6d1619e4c
Francis Billy Raposas <braposas84@gmail.com>
2025-12-20 00:05:04 +0800

    prod_1.1

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -9,11 +9,19 @@ export const appConfig = {
     maxReferenceImages: 10,
     safetySettings: [
       {
-        category: "HARM_CATEGORY_SEXUAL",
+        category: "HARM_CATEGORY_SEXUALLY_EXPLICIT",
         threshold: "BLOCK_MEDIUM_AND_ABOVE",
       },
       {
         category: "HARM_CATEGORY_HATE_SPEECH",
+        threshold: "BLOCK_MEDIUM_AND_ABOVE",
+      },
+      {
+        category: "HARM_CATEGORY_DANGEROUS_CONTENT",
+        threshold: "BLOCK_MEDIUM_AND_ABOVE",
+      },
+      {
+        category: "HARM_CATEGORY_HARASSMENT",
         threshold: "BLOCK_MEDIUM_AND_ABOVE",
       },
     ],
